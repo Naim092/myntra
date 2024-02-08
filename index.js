9b0362375e10125142babc4944fc074824248479
Naim092 <naims2455@gmail.com>
2024-02-08 23:05:42 +1345

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const myntra = require("./myntra");
+const uuid = require("uuid").v4;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.headers["request_id"] = uuid();
+  next();
+});
+
+app.use("/v1/", myntra);
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
+
